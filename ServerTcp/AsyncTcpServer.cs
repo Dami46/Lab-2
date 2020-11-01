eca0157c464a99cc0f9aae48e37c55fbf6503015
Dami46 <56515672+Dami46@users.noreply.github.com>
2020-11-01 01:08:21 +0100

    Many sessions and saving to file added

--- ServerTcp/AsyncTcpServer.cs
+++ ServerTcp/AsyncTcpServer.cs
@@ -19,21 +19,29 @@ namespace ServerTcp
         byte[] msg = new byte[256];
         byte[] msg2 = new byte[256];
         byte[] msg3 = new byte[256];
+        byte[] msg4 = new byte[256];
         StreamReader streamReader;
+        string one = " Zgadnij liczbe od 0 do 9 \r \n";
+        string two = " Brawo udalo ci sie zgadnac liczbe! \r \n";
+        string three = " Wybrano zla wartosc \r \n";
+        string four = "Chcesz kontynuowac rozgrywke ? (1-tak 0-nie) \r \n";
+        string five = "Rozgrywka zakończona \r \n";
+
 
         /// <summary>
         /// Konstruktor domyślny klasy
         /// </summary>
         public AsyncTcpServer(IPAddress IP, int port) : base(IP, port)
         {
-            this.guess = new ASCIIEncoding().GetBytes(" Zgadnij liczbe od 0 do 9 \r \n");
-            this.msg = new ASCIIEncoding().GetBytes(" Brawo udalo ci sie zgadnac liczbe! \r \n");
-            this.msg2 = new ASCIIEncoding().GetBytes(" Wybrano zla wartosc \r \n");
-            this.msg3 = new ASCIIEncoding().GetBytes("Chcesz kontynuowac rozgrywke ? (1-tak 0-nie) \r \n");
+            this.guess = new ASCIIEncoding().GetBytes(one);
+            this.msg = new ASCIIEncoding().GetBytes(two);
+            this.msg2 = new ASCIIEncoding().GetBytes(three);
+            this.msg3 = new ASCIIEncoding().GetBytes(four);
+            this.msg4 = new ASCIIEncoding().GetBytes(five);
         }
         public delegate void TransmissionDataDelegate(NetworkStream stream);
 
-    
+
         protected override void AcceptClient()
         {
             while (true)
@@ -44,7 +52,7 @@ namespace ServerTcp
                 transmissionDelegate.BeginInvoke(networkStream, TransmissionCallback, tcpClient);
             }
         }
-       
+
         private void TransmissionCallback(IAsyncResult ar)
         {
             tcpClient.Close();
@@ -62,24 +70,27 @@ namespace ServerTcp
 
         protected override void BeginDataTransmission(NetworkStream stream)
         {
+            StringBuilder gameLog = new StringBuilder();
             Random random = new Random();
             int number = random.Next(10);
             int nextGame = 1;
-
             Console.WriteLine("Number to guess: " + number);
+            gameLog.Append("Number to guess: " + number + Environment.NewLine);
             while (nextGame == 1)
             {
                 try
                 {
                     Console.WriteLine("Client connected");
+                    gameLog.Append("Client connected  \n" + Environment.NewLine);
                     while (tcpClient.Connected)
                     {
                         streamReader = new StreamReader(networkStream);
                         networkStream.Write(guess, 0, guess.Length);
-
+                        gameLog.Append(one + Environment.NewLine);
                         var guessedVal = streamReader.ReadLine();
                         String time = DateTime.Now.ToString("h:mm:ss");
                         Console.WriteLine(time + " -> " + guessedVal);
+                        gameLog.Append(time + " -> " + guessedVal + Environment.NewLine);
                         int guessedVal2;
                         try
                         {
@@ -93,21 +104,62 @@ namespace ServerTcp
                         if (guessedVal2 > 11 || guessedVal2 < 0)
                         {
                             networkStream.Write(msg2, 0, msg2.Length);
+                            gameLog.Append(three + Environment.NewLine);
                         }
 
                         if (number.Equals(guessedVal2))
                         {
                             networkStream.Write(msg, 0, msg.Length);
+                            gameLog.Append(two + Environment.NewLine);
+                            Console.WriteLine("Client guessed the number");
+                            gameLog.Append("Client guessed the number " + Environment.NewLine);
 
                             networkStream.Write(msg3, 0, msg3.Length);
+                            gameLog.Append(four + Environment.NewLine);
+                            var continueGame = streamReader.ReadLine();
+                            nextGame = Int32.Parse(continueGame);
+                            if (nextGame == 0)
+                            {
+                                networkStream.Write(msg4, 0, msg4.Length);
+                                gameLog.Append(five + Environment.NewLine);
+                                saveGameToFile(gameLog);
+                                tcpClient.Close();
+                                break;
+                            }
+                            else
+                            {
+                                number = random.Next(10);
+                                Console.WriteLine("Number to guess: " + number);
+                                gameLog.Append("Number to guess: " + number + Environment.NewLine);
 
-                            number = random.Next(10);
-                            Console.WriteLine("Number to guess: " + number);
+                            }
                         }
                     }
-                }  catch (Exception ex) {
-                  Console.WriteLine(ex);
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Zaden klient nie jest polonczony z serwerem");
+                    gameLog.Append("Zaden klient nie jest polonczony z serwerem" + Environment.NewLine);
+                }
+            }
+        }
+
+        public void saveGameToFile(StringBuilder gameLog)
+        {
+            String path = Directory.GetCurrentDirectory();
+            String filename = path + @"\result.txt";
+            int i = 1;
+            while (File.Exists(filename))
+            {
+                filename = path + @"\result" + i + @".txt";
+                i++;
+            }
+            using (FileStream fs = File.Create(filename))
+            {
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filename, true))
+            {
+                streamWriter.WriteLine(gameLog);
             }
         }
     }
